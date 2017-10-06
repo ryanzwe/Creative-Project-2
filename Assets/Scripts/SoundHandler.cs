53ed195f61dfa173ec80efd99aef1a4748054f51
ryanzwe <ryanwilton@live.com.au>
2017-10-06 14:44:24 +1000

    Bug fixes to allow build
    - Removed unityeditor namespace
    -added plants

--- Assets/Scripts/SoundHandler.cs
+++ Assets/Scripts/SoundHandler.cs
@@ -34,7 +34,64 @@ public class SoundHandler : MonoBehaviour
         audio.clip = SoundClips[(int) sound];
         audio.Play();
     }
+/*
 
+    [SerializeField]
+    private AudioSource musicSource;
+
+    [SerializeField]
+    private AudioSource sfxSource;
+
+    Dictionary<string, AudioClip> AudioClips = new Dictionary<string, AudioClip>();
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+        AudioClip[] clips = Resources.LoadAll<AudioClip>("Audio") as AudioClip[];
+        foreach (AudioClip clip in clips)
+        {
+            AudioClips.Add(clip.name, clip);
+        }
+
+        LoadVolume();
+
+
+    }
+
+    public void PlayMusic(string name)
+    {
+        musicSource.clip = AudioClips[name];
+        musicSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        musicSource.Stop();
+    }
+
+    public void PlaySingleSfx(string name)
+    {
+        sfxSource.Stop();
+        sfxSource.PlayOneShot(AudioClips[name]);
+    }
+
+    public void LoadVolume()
+    {
+        // Load audio SFX and Music settings, sets audio sources
+        musicSource.volume = PlayerPrefs.GetFloat("Music", 0.4f);
+        sfxSource.volume = PlayerPrefs.GetFloat("SFX", 0.8f);
+
+        for (int i = 0; i < sources.Count; i++)
+        {
+            sources[i].GetComponent<AudioSource>().volume = sfxSource.volume;
+        }
+
+    }
+
+}
+*/
 
 
 }
